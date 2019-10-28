833689ef1ec7d54d39b68ce2f670b9d15d1a616b
Lucas Bruzzone <lucas@neocoast.com>
2019-10-28 14:46:15 -0300

    Add Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
+  globals: {
+    'jest': false
+  },
   extends: '@react-native-community',
 };
---
+++ jest/setup.js
@@ -0,0 +1,31 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+jest.mock('react-navigation', () => ({
+  createAppContainer: jest
+    .fn()
+    .mockReturnValue(function NavigationContainer(props) {
+      return null;
+    }),
+  createDrawerNavigator: jest.fn(),
+  createMaterialTopTabNavigator: jest.fn(),
+  createStackNavigator: jest.fn(),
+  StackActions: {
+    push: jest.fn().mockImplementation(x => ({...x, type: 'Navigation/PUSH'})),
+    replace: jest
+      .fn()
+      .mockImplementation(x => ({...x, type: 'Navigation/REPLACE'})),
+  },
+  NavigationActions: {
+    navigate: jest.fn().mockImplementation(x => x),
+  },
+}));
+
+jest.mock('react-navigation-stack', () => ({
+  Header: {},
+  createStackNavigator: () => {},
+}));
