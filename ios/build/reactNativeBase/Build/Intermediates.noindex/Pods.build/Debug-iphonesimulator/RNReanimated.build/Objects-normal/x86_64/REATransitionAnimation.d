0cb17b3c465ab9112f790c0927beec297269b37e
Lucas Bruzzone <lucas@neocoast.com>
2019-11-04 12:38:13 -0300

    Fix react-native run-android

---
+++ ios/build/reactNativeBase/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/RNReanimated.build/Objects-normal/x86_64/REATransitionAnimation.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/neocoastsrl/Desktop/ReactNative/node_modules/react-native-reanimated/ios/Transitioning/REATransitionAnimation.m \
+  /Users/neocoastsrl/Desktop/ReactNative/ios/Pods/Target\ Support\ Files/RNReanimated/RNReanimated-prefix.pch \
+  /Users/neocoastsrl/Desktop/ReactNative/node_modules/react-native-reanimated/ios/Transitioning/REATransitionAnimation.h
---
+++ ios/build/reactNativeBase/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/RNScreens.build/Objects-normal/x86_64/RNScreens-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/neocoastsrl/Desktop/ReactNative/ios/Pods/Target\ Support\ Files/RNScreens/RNScreens-dummy.m \
+  /Users/neocoastsrl/Desktop/ReactNative/ios/Pods/Target\ Support\ Files/RNScreens/RNScreens-prefix.pch
